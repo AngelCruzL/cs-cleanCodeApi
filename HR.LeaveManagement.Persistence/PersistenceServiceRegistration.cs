fa8cf68edea8106b8c8c413f6a6fd0deead7de2f
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-03 14:06:41 -0600

    Add Persistence project and PersistenceServiceRegistration class

---
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+  public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+  {
+    return services;
+  }
+}

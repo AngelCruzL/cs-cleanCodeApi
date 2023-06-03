886c64916d95626d0b9a33ec40bf9927ef03b485
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-03 13:51:47 -0600

    Add BadRequest custom exception

---
+++ HR.LeaveManagement.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,19 @@
+using FluentValidation.Results;
+
+namespace HR.LeaveManagement.Application.Exceptions;
+
+public class BadRequestException : Exception
+{
+  public BadRequestException(string message) : base(message)
+  {
+  }
+
+  public BadRequestException(string message, ValidationResult validationResult) : base(message)
+  {
+    ValidationErrors = new List<string>();
+
+    foreach (var error in validationResult.Errors) ValidationErrors.Add(error.ErrorMessage);
+  }
+
+  public List<string> ValidationErrors { get; set; }
+}

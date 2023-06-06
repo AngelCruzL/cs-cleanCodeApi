96a67565eaa422bb99a385073872b56220ea0e44
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-05 21:02:40 -0600

    Implement App logger with Update and Get Leave types

--- HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Logging;
 using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
 using MediatR;
 
 namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType;
@@ -7,17 +9,30 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeave
 public class UpdateLeaveTypeCommandHandler : IRequestHandler<UpdateLeaveTypeCommand, Unit>
 {
   private readonly ILeaveTypeRepository _leaveTypeRepository;
+  private readonly IAppLogger<UpdateLeaveTypeCommandHandler> _logger;
   private readonly IMapper _mapper;
 
-  public UpdateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+  public UpdateLeaveTypeCommandHandler(
+    IMapper mapper,
+    ILeaveTypeRepository leaveTypeRepository,
+    IAppLogger<UpdateLeaveTypeCommandHandler> logger)
   {
     _mapper = mapper;
     _leaveTypeRepository = leaveTypeRepository;
+    _logger = logger;
   }
 
   public async Task<Unit> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
   {
     // Validate incoming data
+    var validator = new UpdateLeaveTypeCommandValidator(_leaveTypeRepository);
+    var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+    if (validationResult.Errors.Any())
+    {
+      _logger.LogWarning($"Validation errors in update request for {nameof(LeaveType)} - {request.Id}");
+      throw new BadRequestException("Invalid Leave type", validationResult);
+    }
 
     // Convert to domain entity object
     var leaveTypeToUpdate = _mapper.Map<Domain.LeaveType>(request);
--- HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Logging;
 using HR.LeaveManagement.Application.Contracts.Persistence;
 using MediatR;
 
@@ -7,18 +8,32 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveT
 public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
 {
   private readonly ILeaveTypeRepository _leaveTypeRepository;
+  private readonly IAppLogger<GetLeaveTypesQueryHandler> _logger;
   private readonly IMapper _mapper;
 
-  public GetLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+  public GetLeaveTypesQueryHandler(
+    IMapper mapper,
+    ILeaveTypeRepository leaveTypeRepository,
+    IAppLogger<GetLeaveTypesQueryHandler> logger
+  )
   {
     _mapper = mapper;
     _leaveTypeRepository = leaveTypeRepository;
+    _logger = logger;
   }
 
   public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
   {
+    // Query the database
     var leaveTypes = await _leaveTypeRepository.GetAsync();
 
-    return _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+    // Convert data objects to DTO objects 
+    var data = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+
+    // Log the data
+    _logger.LogInformation("Leave types retrieved successfully");
+
+    // Return the list of DTO objects
+    return data;
   }
 }

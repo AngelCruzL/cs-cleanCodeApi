8068c30b7a48262bd2107df79d35edd3a0d6e660
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-05 21:19:57 -0600

    Set up Api project

--- HR.LeaveManagement.Api/Program.cs
+++ HR.LeaveManagement.Api/Program.cs
@@ -1,8 +1,25 @@
+using HR.LeaveManagement.Application;
+using HR.LeaveManagement.Infrastructure;
+using HR.LeaveManagement.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddPersistenceServices(builder.Configuration);
+
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("all", builder => builder.AllowAnyOrigin()
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+  );
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

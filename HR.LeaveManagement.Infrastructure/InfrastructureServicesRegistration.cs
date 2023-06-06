f66472c62567e70ea057d05c9ff8c4f39ce4f9e1
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-05 20:31:21 -0600

    Set up app logger

--- HR.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
+++ HR.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,6 +1,8 @@
 using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Contracts.Logging;
 using HR.LeaveManagement.Application.Models.Email;
 using HR.LeaveManagement.Infrastructure.EmailService;
+using HR.LeaveManagement.Infrastructure.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,6 +15,7 @@ public static class InfrastructureServicesRegistration
   {
     services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
     services.AddTransient<IEmailSender, EmailSender>();
+    services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
 
     return services;
   }
---
+++ HR.LeaveManagement.Infrastructure/Logging/LoggerAdapter.cs
@@ -0,0 +1,24 @@
+using HR.LeaveManagement.Application.Contracts.Logging;
+using Microsoft.Extensions.Logging;
+
+namespace HR.LeaveManagement.Infrastructure.Logging;
+
+public class LoggerAdapter<T> : IAppLogger<T>
+{
+  private readonly ILogger<T> _logger;
+
+  public LoggerAdapter(ILoggerFactory loggerFactory)
+  {
+    _logger = loggerFactory.CreateLogger<T>();
+  }
+
+  public void LogInformation(string message, params object[] args)
+  {
+    _logger.LogInformation(message, args);
+  }
+
+  public void LogWarning(string message, params object[] args)
+  {
+    _logger.LogWarning(message, args);
+  }
+}

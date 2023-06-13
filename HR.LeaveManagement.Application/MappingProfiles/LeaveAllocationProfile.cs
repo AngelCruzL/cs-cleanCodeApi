3f4c6c7d3dc11d52f16d4d89f11a374eb4b2f7ec
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-13 11:18:28 -0600

    Add LeaveAllocationProfile

---
+++ HR.LeaveManagement.Application/MappingProfiles/LeaveAllocationProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetails;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocations;
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.MappingProfiles;
+
+public class LeaveAllocationProfile : Profile
+{
+  public LeaveAllocationProfile()
+  {
+    CreateMap<LeaveAllocationDto, LeaveAllocation>().ReverseMap();
+    CreateMap<LeaveAllocation, LeaveAllocationDetailsDto>();
+  }
+}
